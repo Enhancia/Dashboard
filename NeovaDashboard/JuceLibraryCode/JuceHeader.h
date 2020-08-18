73d61f571b7ab31f92688a5b19bdc01338bf2a0d
alexlevacher <a.levacher@hotmail.fr>
2020-08-18 15:53:37 +0200

    Changed to 0.3.14

--- NeovaDashboard/JuceLibraryCode/JuceHeader.h
+++ NeovaDashboard/JuceLibraryCode/JuceHeader.h
@@ -27,7 +27,6 @@
 #include <juce_gui_extra/juce_gui_extra.h>
 #include <juce_opengl/juce_opengl.h>
 
-#include "BinaryData.h"
 
 #if defined (JUCE_PROJUCER_VERSION) && JUCE_PROJUCER_VERSION < JUCE_VERSION
  /** If you've hit this error then the version of the Projucer that was used to generate this project is
@@ -49,7 +48,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "NeovaDashboard";
     const char* const  companyName    = "";
-    const char* const  versionString  = "0.3.13";
-    const int          versionNumber  = 0x30d;
+    const char* const  versionString  = "0.3.14";
+    const int          versionNumber  = 0x30e;
 }
 #endif
