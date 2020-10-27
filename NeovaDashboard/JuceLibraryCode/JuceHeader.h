cd552388fb0098d245c42acd90eb637ca306ab27
unknown <a.levacher@hotmail.fr>
2020-10-28 00:08:35 +0100

    Changed to 1.0.0

--- NeovaDashboard/JuceLibraryCode/JuceHeader.h
+++ NeovaDashboard/JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "NeovaDashboard";
     const char* const  companyName    = "";
-    const char* const  versionString  = "101.0.0";
-    const int          versionNumber  = 0x650000;
+    const char* const  versionString  = "1.0.0";
+    const int          versionNumber  = 0x10000;
 }
 #endif
